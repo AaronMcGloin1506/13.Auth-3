a90c1ee18c5d8c6b3503c38d6669206a69e1be81
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-11-10 08:23:28 +0000

    Registering a User

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const app = express();
 
-const mongoUri = 'mongodb+srv://x:x@cluster0.omxue.mongodb.net/awesomeapp?retryWrites=true&w=majority';
+const mongoUri = 'mongodb+srv://x:x@cluster0.omxue.mongodb.net/authApp?retryWrites=true&w=majority';
 mongoose.connect(mongoUri,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,6 +16,20 @@ app.use(bodyParser.json());
 
 ///// Models /////
 const { User } = require('./models/user');
+
+///// Routes /////
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save((err,doc)=>{
+        if(err){
+            res.status(400).send(err)
+        }
+        res.status(200).send(doc)
+    })
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
